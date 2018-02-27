fc3374afb340379cc53461b4fd53341eea693ca5
stedaho <github@stedaho.de>
2018-02-27 20:32:25 +0100

    * Leeres Programm hinzugefügt

---
+++ simple-loco-control.slave/simple-loco-control.slave.ino
@@ -0,0 +1,16 @@
+/* Copyright 2018 Stefan Daniel Homfeld, www.stedaho.de */
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial);
+
+  Serial.println("Starting up...");
+
+
+  Serial.println("Board started");
+}
+
+void loop() {
+
+}
+
