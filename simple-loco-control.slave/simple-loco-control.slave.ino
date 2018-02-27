da258de8fe4169c0706ee13d914e0be6c9cb0d6f
stedaho <github@stedaho.de>
2018-02-27 21:19:20 +0100

    * Button-Behandlungen hinzugefügt: Sobald ein Button gedrückt wird, geht seine LED aus und er bleibt für eine vorgegebene Zeit verriegelt * Blinker als Status-LED hinzugefügt

--- simple-loco-control.slave/simple-loco-control.slave.ino
+++ simple-loco-control.slave/simple-loco-control.slave.ino
@@ -1,10 +1,25 @@
 /* Copyright 2018 Stefan Daniel Homfeld, www.stedaho.de */
 
-const int pinButtonStart = 2;
-const int pinLedStart = 5;
-const int pinButtonStop = 3;
-const int pinLedStop = 6;
-const int pinTransmitter = 4;
+/* Pin-Zuordnungen */
+const byte pinButtonStart = 2;
+const byte pinLedStart = 5;
+const byte pinButtonStop = 3;
+const byte pinLedStop = 6;
+const byte pinTransmitter = 4;
+
+volatile bool buttonPressedFlagStart = false;
+volatile bool buttonPressedFlagStop = false;
+volatile bool buttonLockedFlagStart = false;
+volatile bool buttonLockedFlagStop = false;
+
+unsigned long timePressedStart = 0;
+unsigned long timePressedStop = 0;
+unsigned long timeLedBlinker = 0;
+
+const long buttonDelay = 5000;
+const long blinkerInterval = 500;
+
+bool blinkerState = true;
 
 void setup() {
   Serial.begin(115200);
@@ -17,19 +32,69 @@ void setup() {
   pinMode(pinTransmitter, OUTPUT);
   pinMode(pinButtonStart, INPUT_PULLUP);
   pinMode(pinButtonStop, INPUT_PULLUP);
+  pinMode(LED_BUILTIN, OUTPUT);
   attachInterrupt(digitalPinToInterrupt(pinButtonStart), buttonPressedStart, FALLING);
   attachInterrupt(digitalPinToInterrupt(pinButtonStop), buttonPressedStop, FALLING);
+  
+  digitalWrite(pinLedStart, HIGH);
+  digitalWrite(pinLedStop, HIGH);
+  digitalWrite(LED_BUILTIN, blinkerState);
 
   Serial.println("Board started");
 }
 
 void loop() {
+  unsigned long currentMillis = millis();
 
+  /* Pruefen, ob der Start-Timer abgelaufen ist und ggf. den Button wieder freigeben */
+  if (buttonLockedFlagStart && !buttonPressedFlagStart) {
+    if (currentMillis - timePressedStart > buttonDelay) {
+      digitalWrite(pinLedStart, HIGH);
+      buttonLockedFlagStart = false;
+    }
+  }
+  /* Start-Button wurde gedrueckt (und ist nicht gesperrt) */
+  if (buttonLockedFlagStart && buttonPressedFlagStart) {
+    timePressedStart = currentMillis;
+    digitalWrite(pinLedStart, LOW);
+    Serial.println("Start gedrueckt");
+    buttonPressedFlagStart = false;
+  }
+
+  /* Pruefen, ob der Stop-Timer abgelaufen ist und ggf. den Button wieder freigeben */
+  if (buttonLockedFlagStop && !buttonPressedFlagStop) {
+    if (currentMillis - timePressedStop > buttonDelay) {
+      digitalWrite(pinLedStop, HIGH);
+      buttonLockedFlagStop = false;
+    }
+  }
+  /* Stop-Button wurde gedrueckt (und ist nicht gesperrt) */
+  if (buttonLockedFlagStop && buttonPressedFlagStop) {
+    timePressedStop = currentMillis;
+    digitalWrite(pinLedStop, LOW);
+    Serial.println("Stop gedrueckt");
+    buttonPressedFlagStop = false;
+  }
+
+  /* LED-Status-Blinker */
+  if (currentMillis - timeLedBlinker >= blinkerInterval) {
+    timeLedBlinker = currentMillis;
+    blinkerState = !blinkerState;    
+    digitalWrite(LED_BUILTIN, blinkerState);
+  }
 }
 
 
 void buttonPressedStart() {
+  if (!buttonLockedFlagStart) {
+    buttonLockedFlagStart = true;
+    buttonPressedFlagStart = true;
+  }
 }
 
 void buttonPressedStop() {
+  if (!buttonLockedFlagStop) {
+    buttonLockedFlagStop = true;
+    buttonPressedFlagStop = true;
+  }
 }
