c6345c5fd8da0f49a9a504a6c04c6b0fa0aae984
stedaho <github@stedaho.de>
2018-02-27 20:45:52 +0100

    * Pins initialisiert * ISRs angelegt

--- simple-loco-control.slave/simple-loco-control.slave.ino
+++ simple-loco-control.slave/simple-loco-control.slave.ino
@@ -1,11 +1,24 @@
 /* Copyright 2018 Stefan Daniel Homfeld, www.stedaho.de */
 
+const int pinButtonStart = 2;
+const int pinLedStart = 5;
+const int pinButtonStop = 3;
+const int pinLedStop = 6;
+const int pinTransmitter = 4;
+
 void setup() {
   Serial.begin(115200);
   while (!Serial);
 
   Serial.println("Starting up...");
 
+  pinMode(pinLedStart, OUTPUT);
+  pinMode(pinLedStop, OUTPUT);
+  pinMode(pinTransmitter, OUTPUT);
+  pinMode(pinButtonStart, INPUT_PULLUP);
+  pinMode(pinButtonStop, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(pinButtonStart), buttonPressedStart, FALLING);
+  attachInterrupt(digitalPinToInterrupt(pinButtonStop), buttonPressedStop, FALLING);
 
   Serial.println("Board started");
 }
@@ -14,3 +27,9 @@ void loop() {
 
 }
 
+
+void buttonPressedStart() {
+}
+
+void buttonPressedStop() {
+}
