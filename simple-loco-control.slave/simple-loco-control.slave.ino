18d7ce0271e39cc8d527204cb843fcaf559c858f
stedaho <github@stedaho.de>
2018-02-28 22:57:55 +0100

    * Bibliothek zum Senden von Daten eingebunden und einen einfachen Versand hinzugefügt

--- simple-loco-control.slave/simple-loco-control.slave.ino
+++ simple-loco-control.slave/simple-loco-control.slave.ino
@@ -1,5 +1,7 @@
 /* Copyright 2018 Stefan Daniel Homfeld, www.stedaho.de */
 
+#include <RFTransmitter.h> /* Siehe https://github.com/zeitgeist87/RFTransmitter */
+
 /* Pin-Zuordnungen */
 const byte pinButtonStart = 2;
 const byte pinLedStart = 5;
@@ -7,11 +9,13 @@ const byte pinButtonStop = 3;
 const byte pinLedStop = 6;
 const byte pinTransmitter = 4;
 
+/* Flags zur Abhandlung und dem Sperren der Tasterdrücke */
 volatile bool buttonPressedFlagStart = false;
 volatile bool buttonPressedFlagStop = false;
 volatile bool buttonLockedFlagStart = false;
 volatile bool buttonLockedFlagStop = false;
 
+/* Zeitstempel zur Sperre der Tasterdrücke und den Blinker */
 unsigned long timePressedStart = 0;
 unsigned long timePressedStop = 0;
 unsigned long timeLedBlinker = 0;
@@ -20,6 +24,12 @@ const long buttonDelay = 5000;
 const long blinkerInterval = 500;
 
 bool blinkerState = true;
+
+const byte transmitterId = 28;
+RFTransmitter transmitter(pinTransmitter, transmitterId);
+const byte buttonPressedMessageStart[5] = { 28, 1, 1, 51 };
+const byte buttonPressedMessageStop[5] = { 28, 2, 1, 51 };
+
 
 void setup() {
   Serial.begin(115200);
@@ -35,7 +45,7 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   attachInterrupt(digitalPinToInterrupt(pinButtonStart), buttonPressedStart, FALLING);
   attachInterrupt(digitalPinToInterrupt(pinButtonStop), buttonPressedStop, FALLING);
-  
+
   digitalWrite(pinLedStart, HIGH);
   digitalWrite(pinLedStop, HIGH);
   digitalWrite(LED_BUILTIN, blinkerState);
@@ -57,6 +67,7 @@ void loop() {
   if (buttonLockedFlagStart && buttonPressedFlagStart) {
     timePressedStart = currentMillis;
     digitalWrite(pinLedStart, LOW);
+    transmitter.send((byte *)buttonPressedMessageStart, sizeof(buttonPressedMessageStart));
     Serial.println("Start gedrueckt");
     buttonPressedFlagStart = false;
   }
@@ -72,6 +83,7 @@ void loop() {
   if (buttonLockedFlagStop && buttonPressedFlagStop) {
     timePressedStop = currentMillis;
     digitalWrite(pinLedStop, LOW);
+    transmitter.send((byte *)buttonPressedMessageStop, sizeof(buttonPressedMessageStop));
     Serial.println("Stop gedrueckt");
     buttonPressedFlagStop = false;
   }
@@ -79,7 +91,7 @@ void loop() {
   /* LED-Status-Blinker */
   if (currentMillis - timeLedBlinker >= blinkerInterval) {
     timeLedBlinker = currentMillis;
-    blinkerState = !blinkerState;    
+    blinkerState = !blinkerState;
     digitalWrite(LED_BUILTIN, blinkerState);
   }
 }
